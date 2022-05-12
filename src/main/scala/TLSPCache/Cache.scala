eabbc2add491ab6727844fdf9d44fc6404619f28
Liu Xiaoyi <circuitcoder0@gmail.com>
2022-05-12 20:49:52 +0800

    MSHR allocation and resp

--- src/main/scala/TLSPCache/Cache.scala
+++ src/main/scala/TLSPCache/Cache.scala
@@ -32,12 +32,27 @@ object ArbiteredRequestType extends ChiselEnum {
   val SlaveProbe = Value
 }
 
-class ArbiteredRequest[Bypass <: Data](implicit override val params: Params[Bypass]) extends PreflightRequest[Bypass] {
+class ArbiteredRequest[Bypass <: Data](implicit val params: Params[Bypass]) {
+  val addr = UInt(params.addrWidth.W)
   // Additionally, contains operations from L2
   val ty = ArbiteredRequestType()
   val toCap = CapParam()
   // Is this a probeBlock? Used to assert not modified
   val debugWithBlock = Bool()
+}
+
+class MSHRAllocRequest[Bypass <: Data](implicit val params: Params[Bypass]) extends Bundle {
+  val req = new MainRequest
+  val assoc = UInt(log2Up(params.assocCnt).W)
+
+  val hasRelease = Bool()
+  val releaseCap = PruneReportParam()
+  val releaseTag = UInt(params.tagWidth.W)
+  def hasReleaseData = releaseCap === PruneReportParam.TtoN
+
+  val hasAcquire = Bool()
+  val acquireCap = GrowParam()
+  def hasAcquireData = acquireCap =/= GrowParam.BtoT
 }
 
 class Cache[Bypass <: Data](implicit val params: Params[Bypass]) extends Module {
@@ -49,60 +64,116 @@ class Cache[Bypass <: Data](implicit val params: Params[Bypass]) extends Module 
 
   val resp = IO(Valid(new Response[Bypass]))
 
+  /**
+   * Storages
+   */
   val banks = for(i <- 0 until params.bankCnt) yield new Bank(s"Bank $i")
   val mshrs = for(i <- 0 until params.missesUnderHit) yield (new MSHR).suggestName(s"MSHR $i")
+
+  val mshr_alloc = Decoupled(new MSHRAllocRequest[Bypass])
 
   /**
     * Helpers
     */
-  val victimGen = chisel3.util.random.GaloisLFSR.maxPeriod(log2Up(params.assocCnt))
+  val victim_gen = chisel3.util.random.GaloisLFSR.maxPeriod(log2Up(params.assocCnt))
 
   /**
     * Stage control signals
     */
   val s0_flow = Wire(Bool())
   val s1_flow = Wire(Bool())
-  
+  val s2_flow = Wire(Bool())
 
   val s0_valid = Wire(Bool())
   val s1_valid = RegEnable(s0_valid && s0_flow, s1_flow, false.B)
+  val s2_valid = RegEnable(s1_valid && s1_flow, s2_flow, false.B)
 
   /**
     * Stage 0
+    * 
+    * Fire request into metadata file
     */
   val s0_addr = preflight.bits.vaddr.asTypeOf(new Addr)
   val s0_idx = s0_addr.idx
 
-  val s0_valid_readout = banks(0).validBits(s0_idx)
+  val s0_valids = banks(0).validBits(s0_idx)
   s0_valid := preflight.ready
   s0_flow := s1_flow || !s1_valid
+  preflight.ready := s0_flow
 
   /**
     * Stage 1
     */
   val s1_addr = main.paddr.asTypeOf(new Addr)
-  val s1_tags = banks(0).tags.read(s0_idx)
+  val s1_tags_readout = banks(0).tags.read(s0_idx)
+  val s1_tags = Wire(s1_tags_readout.cloneType)
+  s1_tags := Mux(RegNext(s1_flow), s1_tags_readout, RegNext(s1_tags))
+
+  val s1_bypass = RegEnable(preflight.bits.bypass, s1_flow)
   val s1_idx = RegEnable(s0_idx, s1_flow)
   assert(s1_idx === s1_addr.idx)
-  val s1_valid_readout = RegEnable(s0_valid_readout, s1_flow)
-  val s1_hitmap = s1_tags.map(_ === s1_addr.tag).zip(s1_valid_readout).map({ case (l, r) => l && r })
+  val s1_valids = RegEnable(s0_valids, s1_flow)
+  val s1_hitmap = s1_tags.map(_ === s1_addr.tag).zip(s1_valids).map({ case (l, r) => l && r })
   val s1_hit_assoc = OHToUInt(s1_hitmap)
   val s1_hit = VecInit(s1_hitmap).asUInt.orR
+  val s1_full = VecInit(s1_hitmap).asUInt.andR
   val debug_s1_hit_cnt = PopCount(s1_hitmap)
   assert(!s1_valid || debug_s1_hit_cnt <= 1.U)
 
   // Victim or hit assoc
-  val s1_assoc = Mux(s1_hit, s1_hit_assoc, victimGen)
+  val s1_victim = Mux(s1_full, victim_gen, PriorityEncoder(VecInit(s1_hitmap.map(!_)).asUInt))
+  val s1_victim_tag = s1_tags(victim_gen)
+  val s1_victim_dirty = banks(0).dirtyBits(s1_idx)(victim_gen)
 
-  val s1_data_idx = s1_addr.idx ## s1_addr.offset ## s1_assoc
+  val s1_data_idx = s1_addr.idx ## s1_addr.offset ## s1_hit_assoc
   // Read / write port for data
-  val s2_data_readout = banks(0).data.read(s1_data_idx)
   val s1_has_write = main.we.asUInt.orR
-  when(s1_has_write && s1_flow && s1_hit) {
+
+  // TODO: before write, check permission
+  // TODO: use a arbiter to arbiter between MSHR read
+  when(s1_has_write && s1_valid && s1_flow && s1_hit) {
     banks(0).data.write(s1_data_idx, main.wdataAsWrites, main.we)
   }
   // Override dirty bits
   for(i <- 0 until params.assocCnt) {
-    banks(0).dirtyBits(s1_idx)(i) := s1_flow && s1_hitmap(i) && s1_has_write || banks(0).dirtyBits(s1_idx)(i)
+    banks(0).dirtyBits(s1_idx)(i) := s1_valid && s1_flow && s1_hitmap(i) && s1_has_write || banks(0).dirtyBits(s1_idx)(i)
   }
+
+  s1_flow := s2_flow || !s2_valid
+
+  /**
+    * Stage 2
+    */
+
+  // If not hit, readout is undefined. If hit, readout is the result data
+  val s2_data_readout = banks(0).data.read(s1_data_idx)
+  val s2_data = Wire(s2_data_readout.cloneType)
+  s2_data := Mux(RegNext(s2_flow), s2_data_readout, RegNext(s2_data))
+
+  val s2_bypass = RegEnable(s1_bypass, s2_flow)
+  val s2_main = RegEnable(main, s2_flow)
+  val s2_hit = RegEnable(s1_hit, s2_flow)
+  val s2_hit_assoc = RegEnable(s1_hit_assoc, s2_flow)
+  val s2_victim = RegEnable(s1_victim, s2_flow)
+  val s2_victim_tag = RegEnable(s1_victim_tag, s2_flow)
+  val s2_victim_dirty = RegEnable(s1_victim_dirty, s2_flow)
+  val s2_full = RegEnable(s1_full, s2_flow)
+  val s2_has_write = RegEnable(s1_has_write, s2_flow)
+
+  resp.bits.bypass := s2_bypass
+  resp.bits.data := s2_data.asUInt
+  resp.bits.error := false.B
+  // TODO: check if is request from client
+  resp.valid := s2_hit
+
+  mshr_alloc.bits.req := s2_main
+  mshr_alloc.bits.hasRelease := s2_full
+  mshr_alloc.bits.releaseCap := Mux(s2_victim_dirty, PruneReportParam.TtoN, PruneReportParam.BtoN)
+  mshr_alloc.bits.releaseTag := s2_victim_tag
+  mshr_alloc.bits.hasAcquire := true.B // TODO: Implements non-client requests!
+  mshr_alloc.bits.acquireCap := Mux(s2_has_write, GrowParam.NtoT, GrowParam.NtoB) // TODO: implements write with non-sufficient permission
+  
+  mshr_alloc.valid := !s2_hit
+
+  s2_flow := s2_hit || mshr_alloc.ready
 }
